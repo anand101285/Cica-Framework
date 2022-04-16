95e8ae0cd3bd09fde9a440c7b93267e62e9a39f5
anand101285 <anand_talreja1@hotmail.com>
2022-04-16 19:38:51 +0500

    Try 7

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => res.send("API Running"));
 
